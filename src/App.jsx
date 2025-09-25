6f30e989884c8350799693ca7d2e06c4618ed669
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-09-26 02:37:58 +0600

    Add Navbar and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Customer Support Zone</h1>
-        
+      <div className='max-w-[1280px] mx-auto'>
+        <Navbar></Navbar>
       </div>
     </>
   )
